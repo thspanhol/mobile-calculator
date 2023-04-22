1c13d54fe22a7b74a2d243f4874615e7d33cdff0
Thales Spanhol <thales_spanhol@hotmail.com>
2023-04-22 20:44:18 -0300

    calculator mobile

---
+++ assets/components/CalculatorBtn.js
@@ -0,0 +1,47 @@
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+
+const CalculatorBtn = ({text, onPress, disabled}) => {
+
+    return (
+        <TouchableOpacity onPress={onPress} style={styles.box} disabled={disabled}>
+            <View style={['÷', '×', '-', '+'].includes(text) ? (styles.button_exp) : (text === '=' ? styles.button_eq : styles.button_number)}>
+                <Text style={styles.buttonText}>{text}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    box: {
+        width: '24.5%',
+        margin: '0.25%',
+    },
+    button_number: {
+        borderRadius: 15,
+        paddingVertical: 14,
+        paddingHorizontal: 10,
+        backgroundColor: '#252525',
+    },
+    button_exp: {
+        borderRadius: 15,
+        paddingVertical: 14,
+        paddingHorizontal: 10,
+        backgroundColor: 'pink',
+        marginLeft: 2,
+    },
+    button_eq: {
+        borderRadius: 15,
+        paddingVertical: 14,
+        paddingHorizontal: 10,
+        backgroundColor: 'red',
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize: '50%',
+        textAlign: 'center',
+    },
+})
+
+export default CalculatorBtn;
---
+++ assets/components/CalculatorScreen.js
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const CalculatorScreen = ({text}) => {
+
+    return (
+            <View style={styles.screen}>
+                <Text style={styles.text}>{text}</Text>
+            </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        width: '80%',
+      
+        borderRadius: 15,
+        paddingVertical: 14,
+        paddingHorizontal: 10,
+        backgroundColor: '#fff',
+        marginBottom: 5,
+  
+    },
+    text: {
+        height: 50,
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize: 50,
+        textAlign: 'right',
+    color: 'orange',
+    backgroundColor: '#fff',
+    padding: 0,
+    },
+})
+
+export default CalculatorScreen;
